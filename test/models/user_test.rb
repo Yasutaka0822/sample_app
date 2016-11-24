a2b8bff7bf6beafde3ec8e72bea3273614be472b
Yasutaka0822 <y_shimizu@dreamarts.co.jp>
2016-11-24 20:24:37 +0000

    user presence test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  
+# setupメソッド内に書かれた処理は、
+#各テストが走る直前に実行されます。
+#@userはインスタンス変数ですが、
+#setupメソッド内で宣言しておけば、
+#すべてのテスト内でこのインスタンス変数が
+#使えるようになります。
+  def setup
+    @user = User.new(name: "Yasutaka Shimizu", email: "a1415ys@aiit.ac.jp")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name shoud be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
 end
