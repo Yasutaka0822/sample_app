535c37b9e3a28c629225d0a53b3f953e601ae179
Yasutaka0822 <y_shimizu@dreamarts.co.jp>
2016-12-03 05:50:17 +0000

    list 6.23

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase } # アドレスの小文字変換
+  before_save { self.email = email.downcase } # アドレスの小文字
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false } # 一意性の保証
+  has_secure_password
 end
