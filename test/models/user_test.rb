0d932ad46117d02583ea0691174341e0aa7a55d0
Yasutaka0822 <y_shimizu@dreamarts.co.jp>
2016-12-03 06:54:33 +0000

    test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,62 +1,69 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
-  
-# setupメソッド内に書かれた処理は、
-#各テストが走る直前に実行されます。
-#@userはインスタンス変数ですが、
-#setupメソッド内で宣言しておけば、
-#すべてのテスト内でこのインスタンス変数が
-#使えるようになります。
   def setup
-    @user = User.new(name: "Yasutaka Shimizu", email: "a1415ys@aiit.ac.jp")
+    @user = User.new(name: "Example User", email:"user@example.com",
+    password: "foobar", password_confirmation: "foobar")
   end
   
   test "should be valid" do
     assert @user.valid?
   end
   
-  test "name shoud be present" do
-    @user.name = ""
+  test "name should be present" do
+    @user.name = "    "
     assert_not @user.valid?
   end
   
-  test "email should be present" do
-    @user.email = "   "
+  test "email shoud be present" do
+    @user.email = "    "
     assert_not @user.valid?
   end
   
   test "name should not be too long" do
-    @user.name = "a" * 51 #51文字までの名前
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
   
-  test "email shoud not be too long" do
+  test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
   
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@examplel.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
   
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
   
-  #@userと同じメールアドレスのユーザーは作成できないことを、
-  #@user.dupを使ってテストしています。
-  #dupは、同じ属性を持つデータを複製するためのメソッドです。
-  #@userを保存した後では、複製されたユーザーのメールアドレスが
-  #既にデータベース内に存在するため、ユーザの作成は無効になるはずです。
+  # @user と同じメールアドレスを作成させない
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
+    duplicate_user.email = @user.email.upcase # 大文字・小文字の区別
     @user.save
     assert_not duplicate_user.valid?
+  end
+  
+  # パスワードの最小文字
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+  
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
